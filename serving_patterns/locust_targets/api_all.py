333dcb0792eea2bb048e96e239ccdf362317c635
shibuiwilliam <shibuiyusuke@gmail.com>
2020-07-26 17:42:44 +0900

    update

--- serving_patterns/locust_targets/api_all.py
+++ serving_patterns/locust_targets/api_all.py
@@ -4,15 +4,15 @@ from locust import HttpUser, TaskSet, task, between, constant
 class APIUser(HttpUser):
     wait_time = between(1,10)
 
-    @task(1)
+    @task(0)
     def get_health(self):
         self.client.get(url='/health', verify=False)
 
-    @task(1)
+    @task(0)
     def get_predict(self):
         self.client.get(url='/predict', verify=False)
 
-    @task(1)
+    @task(0)
     def get_predict_label(self):
         self.client.get(url='/predict/label', verify=False)
 
